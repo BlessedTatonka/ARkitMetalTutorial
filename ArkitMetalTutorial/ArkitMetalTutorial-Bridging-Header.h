01a07339aaace1ee7dc16eb9e2e9e6112d1e6015
Борис Малашенко <borismalasenko@MacBook-Air-Boris.local>
2020-10-31 12:03:47 +0300

    q

---
+++ ArkitMetalTutorial/ArkitMetalTutorial-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  ArkitMetalTutorial-Bridging-Header.h
+//  ArkitMetalTutorial
+//
+//  Created by Борис Малашенко on 27.10.2020.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
